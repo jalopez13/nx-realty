d458979b0d53e17346a65e3915c6d906372a41be
Javier Lopez <jalopez13@gmail.com>
2022-08-30 15:50:45 -0700

    feat: add jest test configuration for typescript

--- libs/shared/ui/jest.config.ts
+++ libs/shared/ui/jest.config.ts
@@ -9,6 +9,8 @@ export default {
   },
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/fileTransformer.js',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/shared/ui',
